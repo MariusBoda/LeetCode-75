3338e98a63515e0cfc80069a9268ac09878253f4
Marius Boda <marius.boda@aalto.fi>
2024-09-11 20:50:04 +0300

    added files

---
+++ Array-String/Can Place Flowers/605. Can Place Flowers.py
@@ -0,0 +1,19 @@
+Class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+
+        for i in range(len(flowerbed)):
+            if i == 0 or flowerbed[i-1] == 0:
+                left = True
+            else: 
+                left = False
+                                                                                                    if i == len(flowerbed) - 1 or flowerbed[i+1] == 0:
+                                                                                                        right = True
+                                                                                                    else:
+                                                                                                        right = False
+                                                                                                                                                                    if left and right and flowerbed[i] == 0:
+                                                                                                                                                                        flowerbed[i] = 1
+                                                                                                                                                                                                        n -= 1
+
+                                                                                                                                                                                                                            return n <= 0
+
+
